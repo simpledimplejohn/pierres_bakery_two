cdda7b732156c162afd8ce3dd97495ede4075b8c
Simple Dimple John <simpledimplejohn@gmail.com>
2021-07-25 12:47:37 -0700

    pass test 8 for OrderTests create instance of order

---
+++ Bakery.Tests/ModelTests/OderTests.cs
@@ -0,0 +1,23 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bakery.Models;
+using System.Collections.Generic;
+
+namespace Bakery.Tests
+{
+  [TestClass]
+  public class OrderTests : IDisposable
+  {
+    public void Dispose()
+    {
+      Order.ClearAll();
+    }
+
+    [TestMethod]
+    public void OrderConstructor_CreatesInstanceOfOrder_Order()
+    {
+      Order newOrder = new Order("test", "test", 1, "test");
+      Assert.AreEqual(typeof(Order), newOrder.GetType());
+    }
+  }
+}
