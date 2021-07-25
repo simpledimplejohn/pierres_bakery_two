574847ecf0bd8e4771d8fa339541773dea0d9228
Simple Dimple John <simpledimplejohn@gmail.com>
2021-07-25 11:37:35 -0700

    pass vedorTest 1 create vendor constructor

--- Bakery.Tests/ModelTests/VendorTests.cs
+++ Bakery.Tests/ModelTests/VendorTests.cs
@@ -5,12 +5,18 @@ using System;
 
 namespace Bakery.Tests
 {
+  [TestClass]
   public class VendorTests : IDisposable
   {
     public void Dispose()
     {
       Vendor.ClearAll();
     }
-
+    [TestMethod]
+    public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+    {
+      Vendor newVendor = new Vendor("test", "test");
+      Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
   }
 }
